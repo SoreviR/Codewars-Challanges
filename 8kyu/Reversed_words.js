be3dc7a365090f3fe5f06ff5b64e51b4c6555749
SoreviR <rob.riveros.es@gmail.com>
2023-01-16 15:37:00 +0100

    Added two more 8kyu kata challenges

---
+++ 8kyu/Reversed_words.js
@@ -0,0 +1,25 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+function reverseWords(str){
+    var split = str.split(" ");
+    var reverseArray = split.reverse();
+    var joinArray = reverseArray.join(" ");
+    return joinArray; // reverse those words
+}
+
+//-------------------------------------------------------------------------
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+}
+
+//-------------------------------------------------------------------------
+
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
